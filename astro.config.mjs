758869b0a877548efb43cdd1952b26e8d0fc5e84
chrystianlozano <hola@chrystian.dev>
2026-02-09 15:19:12 -0600

    Add GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://chrystianlozano.github.io",
+  base: "/mudo.games",
   server: { port: 4321, host: true }
 });
